7b5bfd0c7d64431cc76c906e75fe74842aa72d21
Elias Neto <eliasneto072@gmail.com>
2026-08-24 18:28:54 -0300

    chore(setup) Initial Setup

---
+++ backend/src/config/env.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+
+export const env = {
+    
+    PORT: Number(process.env.PORT),
+    
+    DATABASE_URL: process.env.DATABASE_URL ?? ''
+}
---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import { app } from './app'
+import { env } from './config/env'
+
+
+const PORT = env.PORT ?? 3000
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+})
